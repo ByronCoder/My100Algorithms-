d5f501236550306db8512b303998b6d3670da3d2
ByronCoder <ByronCoder>
2019-01-04 20:30:33 -0500

    completed some algorithms Jan 4th 2019

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,21 @@
 function adjacentElementsProduct(inputArray: number[]): number {
+    var product = 0;
+    var largestProduct = 0;
+    for(var i=0; i< inputArray.length; i++) {
+        product = inputArray[i] * inputArray[i+1];
+    }
+}
 
+
+// Dylan Solution
+function adjacentElementsProduct2(inputArray: number[]): number {
+    var product = 0;
+    var largestProduct = 0;
+    for(var i=0; i< inputArray.length; i++) {
+        product = inputArray[i] * inputArray[i+1];
+    }
 }
 
 console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+
+console.log(adjacentElementsProduct2([3, 6, -2, -5, 7, 3]));
