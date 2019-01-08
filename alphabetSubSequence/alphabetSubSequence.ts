30aa1882681cfa0905cced25ae4a6eaa81de7cf7
ByronCoder <ByronCoder>
2019-01-07 19:59:10 -0500

    finished some more algorithms 1/7/2019

--- alphabetSubSequence/alphabetSubSequence.ts
+++ alphabetSubSequence/alphabetSubSequence.ts
@@ -1,4 +1,39 @@
 function alphabetSubsequence(s: string): boolean {
+ 
+    //My Solution
+    for (let i = 0; i < s.length; i++) {
+
+        if(s.charCodeAt(i) >= s.charCodeAt(i+ 1))
+        {
+            return false;
+        }
+        
+    }
+
+    return true;
+}
+//Dylan's Solution
+function alphabetSubsequence2(s: string): boolean {
+    const chars : string[] = s.split('');
+    const charValues: number[] = [];
+
+
+    chars.forEach((char: string) => {
+        charValues.push(char.charCodeAt(0))
+    });
+
+    if (new Set(charValues).size !== charValues.length) {
+        return false;
+    }
+
+
+   for(let i = 0; i < charValues.length - 1; i++) {
+       if(charValues[i] >= charValues[i + 1]) {
+           return false;
+       }
+   }
+
+    return true;
 
 }
 
@@ -7,3 +42,10 @@ console.log(alphabetSubsequence('effg'))
 console.log(alphabetSubsequence('cdce'))
 console.log(alphabetSubsequence('ace'))
 console.log(alphabetSubsequence('bxz'))
+
+
+console.log(alphabetSubsequence2('zab'))
+console.log(alphabetSubsequence2('effg'))
+console.log(alphabetSubsequence2('cdce'))
+console.log(alphabetSubsequence2('ace'))
+console.log(alphabetSubsequence2('bxz'))
--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,50 @@
+// My Solution 
 function alternatingSums(a: number[]): number[] {
+    const team1: number[] = [];
+    const team2: number[] = [];
+    const sumArray: number[] = [];
+
+    for (let i = 0; i < a.length; i+=2) {
+        team1.push(a[i]);
+       
+    }
+
+    for (let i = 0; i < a.length -1; i+=2) {
+        team2.push(a[i+1]);
+       
+    }
+
+    var team1Weight =  team1.reduce((a, b) => {
+        return a + b;
+    });
+
+    var team2Weight =  team2.reduce((a, b) => {
+        return a + b;
+    });
+
+    sumArray.push(team1Weight);
+    sumArray.push(team2Weight);
+
+    return sumArray;
+}
+
+
+function alternatingSums2(a: number[]): number[] { 
+    let evenSum = 0;
+    let oddSum = 0;
+
+    a.forEach((element, index) => {
+        if(index % 2 === 0) {
+            evenSum += element;
+        } else {
+            oddSum += element;
+        }
+
+    });
+
+    return [evenSum, oddSum];
 
 }
 
-console.log(alternatingSums([50, 60, 60, 45, 70]))
+console.log(alternatingSums([50, 60, 60, 45, 70]));
+console.log(alternatingSums([50, 60, 60, 45, 70]));
