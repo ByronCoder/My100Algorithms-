8c3e8eb1add1ef949fc6f45493122fe3c361f94f
ByronCoder <ByronCoder>
2019-01-29 19:55:41 -0500

    finished some more algorithms 1/29/2019

--- composeRanges/composeRanges.ts
+++ composeRanges/composeRanges.ts
@@ -1,23 +1,72 @@
-// My solution (unfinished)
+// Nazereth solution
 
 function composeRanges(nums: number[]): string[] {
-    let ranges: number[] = [];
-     ranges[0] = nums[0];
-
-    for(let i = 1; i < nums.length; i++) {
+    let ranges: any[] = [];
     
+    let result: any[] = [];
+
+
+     for (let i = 0; i < nums.length; i++) {
+
         if(nums[i+1] === nums[i] + 1) {
-            console.log(i);
-            ranges.push(nums[i]);
+            result.push(nums[i]);
             
+        } else {
+            result.push(nums[i]);
+            ranges.push(result);
+            result = [];
         }
-            
-        }
-        console.log(ranges);
+
     }
 
+    for(let i = 0; i < ranges.length; i++) {
+        let start: number[] = ranges[i][0];
+        let end : number[]  = ranges[i][ranges[i].length -1];
+        ranges[i] = (ranges[i].length > 1) ?  `${start}->${end}` : `${start}`;
+
+    }
+
+    return ranges;
+}
+        
+        
+         
     
-
-
+  
 
 console.log(composeRanges([-1, 0, 1, 2, 6, 7, 9]));
+
+// Dylan solution
+
+function composeRanges2(nums: number[]): string[] {
+
+    if(nums.length < 1) {
+        return [];
+    }
+
+    const ranges: any[] = [{start: nums[0], end: nums[0]}];
+
+    for(let i = 1; i < nums.length; i++) {
+        if(ranges[ranges.length -1].end + 1 === nums[i]) {
+            ranges[ranges.length -1].end = nums[i];
+        } else {
+            ranges.push({start: nums[i], end: nums[i]});
+        }
+    }
+
+    for(let i = 0; i < ranges.length; i++) {
+        if(ranges[i].start !== ranges[i].end) {
+            ranges[i] = `${ranges[i].start}->${ranges[i].end}`;
+        } else {
+            ranges[i] = ranges[i].start.toString();
+        }
+    }
+
+    return ranges;
+
+}
+
+
+ 
+
+console.log(composeRanges2([-1, 0, 1, 2, 6, 7, 9]));
--- confirmEnding/confirmEnding.ts
+++ confirmEnding/confirmEnding.ts
@@ -1,5 +1,8 @@
+// My solution
+
 function confirmEnding(str: string, target: string) {
    
+    return str.substring(str.length - target.length) === target;
 }
 
 console.log(confirmEnding("Abstraction", "action"));
